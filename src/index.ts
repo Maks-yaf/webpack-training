55284319e6bddabd56b0ecda42f26f7592d909e5
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-06-19 13:14:11 +0300

    connect devServer to webpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import {calc} from "./test";
 
-console.log("hello world!!!!")
+console.log("hello world")
 console.log(calc(4, 51))
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,19 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import type {Configuration as DevServerConfiguration} from 'webpack-dev-server';
 
 type Mode = 'development' | 'production';
 
 interface EnvVariables {
     mode: Mode;
+    port: number
 }
 
 export default (env: EnvVariables) => {
+
+    const isDev = env.mode === 'development';
+
     const config: webpack.Configuration = {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -33,6 +38,11 @@ export default (env: EnvVariables) => {
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
         },
+        devtool: isDev ? 'inline-source-map' : false,
+        devServer: isDev?  {
+            port: env.port ?? 3000,
+            open: true,
+        } : undefined,
     }
     return config;
 
