502b595d9b91f07d40599a3b7d2b4c15c261ee4a
Maks-yaf <maksim.yafimchyk@gamil.com>
2024-06-20 15:29:11 +0300

    decomposition webpackconfig

---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,9 @@
+import {Configuration} from "webpack";
+import {BuildOptions} from "./type/types";
+
+
+export function buildResolvers(options: BuildOptions): Configuration['resolve'] {
+    return {
+        extensions: ['.tsx', '.ts', '.js'],
+    }
+}
